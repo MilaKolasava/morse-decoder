499b4e56127d15ec451f0e2ebce2c5284f8de3d0
Mila Kolasava <kolasava.dev@gmail.com>
2023-01-28 21:35:39 +0100

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -38,8 +38,45 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
-}
+    
+      let result = '';
+      const splittedExpresion = [];
+
+      for (i = 0; i < expr.length; i = i + 10) {
+        splittedExpresion.push(expr.slice(i, i + 10))
+         }
+ 
+         splittedExpresion.forEach( encodedLetter => {
+            let morseLetter = '';
+
+            if (encodedLetter === '**********') {
+                result = result + ' ';
+                return;
+            } else {
+                for (let index = 0; index < encodedLetter.length; index = index + 2) {
+
+                    switch (encodedLetter.slice(index, index + 2)) {
+                        case "00":
+                            morseLetter = morseLetter + '';
+                            break;
+                        case "10":
+                            morseLetter = morseLetter + '.';
+                            break;
+                        case "11":
+                            morseLetter = morseLetter + '-';
+                            break;
+                    }
+                                
+                }
+            }
+            result = result + MORSE_TABLE[morseLetter]
+
+
+         })
+        return result;
+      }
+
+
 
 module.exports = {
     decode
